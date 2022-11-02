3ac108bcbef3bb9f1d3d4a00e4b5ddc903afc681
redaiq90 <60717660+redaiq90@users.noreply.github.com>
2022-11-02 07:34:23 +0300

    Update utils.py

--- jepthon/helpers/functions/utils.py
+++ jepthon/helpers/functions/utils.py
@@ -100,6 +100,25 @@ def deEmojify(inputString: str) -> str:
     return re.sub("[^a-zA-Z0-9 \\`~!@#$%^&*(){}[\]_+=.:;\n'\",><?/-]", "", inputString)
 
 
-def soft_deEmojify(inputString: str) -> str:
-    """Remove emojis and other non-safe characters from string"""
-    return re.sub("[^a-zA-Z0-9 \\`~!@#$%^&*(){}[\]_+=.:;\n'\",><?/-]", "", inputString)
+def soft_deEmojify(data):
+    emoj = re.compile("["
+        u"\U0001F600-\U0001F64F"
+        u"\U0001F300-\U0001F5FF"
+        u"\U0001F680-\U0001F6FF"
+        u"\U0001F1E0-\U0001F1FF"
+        u"\U00002500-\U00002BEF"
+        u"\U00002702-\U000027B0"
+        u"\U00002702-\U000027B0"
+        u"\U000024C2-\U0001F251"
+        u"\U0001f926-\U0001f937"
+        u"\U00010000-\U0010ffff"
+        u"\u2640-\u2642" 
+        u"\u2600-\u2B55"
+        u"\u200d"
+        u"\u23cf"
+        u"\u23e9"
+        u"\u231a"
+        u"\ufe0f"
+        u"\u3030"
+                      "]+", re.UNICODE)
+    return re.sub(emoj, '', data)
