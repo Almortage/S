5e34fe8c99145429c85077190e948f80e275d76a
السيد <106372829+jepthoniq@users.noreply.github.com>
2022-11-02 19:40:51 +0300

    ياعلي

--- jepthon/plugins/منشن.py
+++ jepthon/plugins/منشن.py
@@ -33,37 +33,7 @@ async def menall(event):
     async for usr in jepiq.iter_participants(chat_id):
         if not chat_id in spam_chats:
             break
-        usrtxt = f"{msg}\n\n[{usr.first_name}](tg://user?id={usr.id}) "
-        await jepiq.send_message(chat_id, usrtxt)
-        await asyncio.sleep(1)
-        await event.delete()
-    try:
-        spam_chats.remove(chat_id)
-    except:
-        pass
-@jepiq.ar_cmd(pattern="تحكم(?:\s|$)([\s\S]*)")
-async def menall(event):
-    chat_id = event.chat_id
-    if event.is_private:
-        return await edit_or_reply(event, "** ᯽︙ هذا الامر يستعمل للقنوات والمجموعات فقط !**")
-    msg = event.pattern_match.group(1)
-    if not msg:
-        return await edit_or_reply(event, "** ᯽︙ ضع رسالة للمنشن اولاً**")
-    is_admin = False
-    try:
-        partici_ = await jepiq(GetParticipantRequest(
-          event.chat_id,
-          event.sender_id
-        ))
-    except UserNotParticipantError:
-        is_admin = False
-    spam_chats.append(chat_id)
-    usrnum = 0
-    usrtxt = ''
-    async for usr in jepiq.iter_participants(chat_id):
-        if not chat_id in spam_chats:
-            break
-        usrtxt = f"{msg} [{usr.first_name}](tg://user?id={usr.id}) "
+        usrtxt = f"{msg}\n[{usr.first_name}](tg://user?id={usr.id}) "
         await jepiq.send_message(chat_id, usrtxt)
         await asyncio.sleep(1)
         await event.delete()
