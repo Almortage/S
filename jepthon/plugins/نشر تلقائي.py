965d530e50290b90c504a177f8afa0a1ec69bcef
السيد <106372829+jepthoniq@users.noreply.github.com>
2022-11-09 12:20:22 +0300

    ياعلي مدد

--- jepthon/plugins/نشر تلقائي.py
+++ jepthon/plugins/نشر تلقائي.py
@@ -1,42 +1,105 @@
 from telethon import *
+from telethon.tl import functions, types
+from telethon.tl.functions.channels import GetParticipantRequest, GetFullChannelRequest
+from telethon.errors.rpcerrorlist import UserNotParticipantError
+from telethon.tl.functions.messages import ExportChatInviteRequest
+from telethon.tl.functions.users import GetFullUserRequest
+
 from jepthon import jepiq
-from jepthon.sql_helper.autopost_sql import add_post, get_all_post, is_post, remove_post
+
+from ..Config import Config
+from ..core.managers import edit_delete, edit_or_reply
+from ..sql_helper.autopost_sql import add_post, get_all_post, is_post, remove_post
+from jepthon.core.logger import logging
+from ..sql_helper.globals import gvarstatus
+from . import BOTLOG, BOTLOG_CHATID
 from . import *
 
-@jepiq.on(admin_cmd(pattern="نشر_التلقائي ?(.*)"))
+async def get_user_from_event(event):
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        user_object = await event.client.get_entity(previous_message.sender_id)
+    else:
+        user = event.pattern_match.group(1)
+        if user.isnumeric():
+            user = int(user)
+        if not user:
+            self_user = await event.client.get_me()
+            user = self_user.id
+        if event.message.entities:
+            probable_user_mention_entity = event.message.entities[0]
+            if isinstance(probable_user_mention_entity, MessageEntityMentionName):
+                user_id = probable_user_mention_entity.user_id
+                user_obj = await event.client.get_entity(user_id)
+                return user_obj
+        if isinstance(user, int) or user.startswith("@"):
+            user_obj = await event.client.get_entity(user)
+            return user_obj
+        try:
+            user_object = await event.client.get_entity(user)
+        except (TypeError, ValueError) as err:
+            await event.edit(str(err))
+            return None
+    return user_object
+
+@jepiq.ar_cmd(pattern=f"النشر_التلقائي ?(.*)")
 async def _(event):
     if (event.is_private or event.is_group):
-        return await edit_or_reply(event, "امر التلقائي يستخدم فقط للقنوات")
-    hel_ = event.pattern_match.group(1)
-    if str(hel_).startswith("-100"):
-        jp = str(hel_).replace("-100", "")
+        return await edit_or_reply(event, "**᯽︙ عـذراً .. النشر التلقائي خـاص بالقنـوات فقـط**")
+    jok = event.pattern_match.group(1)
+    if not jok:
+        return await edit_or_reply(event, "**᯽︙ عـذراً .. قـم بـ إضـافة معـرف/ايـدي القنـاة الى الامـر اولاً**")
+    if jok.startswith("@"):
+        joker = jok
+    elif jok.startswith("https://t.me/"):
+        joker = jok.replace("https://t.me/", "@")
+    elif str(jok).startswith("-100"):
+        joker = str(jok).replace("-100", "")
     else:
-        jp = hel_
-    if not jp.isdigit():
-        return await edit_or_reply(event, "**رجاء قم بوضع ايدي القناه بجانب الأمر !!**")
-    if is_post(jp , event.chat_id):
-        return await edit_or_reply(event, "هذا القناه بلفعل متفعل فيها التلقائي.")
-    add_post(jp, event.chat_id)
-    await edit_or_reply(event, f"**📍 تم بدء امر التلقائي لقناه :** `{hel_}`")
+        try:
+            joker = int(jok)
+        except BaseException:
+            return await edit_or_reply(event, "**᯽︙ عـذراً .. معـرف/ايـدي القنـاة غيـر صـالح**\n**✾╎الرجـاء التـأكـد مـن المعـرف/الايـدي**")
+    try:
+        joker = (await event.client.get_entity(joker)).id
+    except BaseException:
+        return await event.reply("**᯽︙ عـذراً .. معـرف/ايـدي القنـاة غيـر صـالح**\n**✾╎الرجـاء التـأكـد مـن المعـرف/الايـدي**")
+    if is_post(str(joker) , event.chat_id):
+        return await edit_or_reply(event, "**᯽︙ النشـر التلقـائي من القنـاة ** `{jok}` **مفعـل مسبقـاً ✓**")
+    add_post(str(joker), event.chat_id)
+    await edit_or_reply(event, f"**᯽︙ تم تفعيـل النشـر التلقـائي من القنـاة ** `{jok}` **بنجـاح ✓**")
 
 
-@jepiq.on(admin_cmd(pattern="ايقاف_النشر ?(.*)"))
+
+@jepiq.ar_cmd(pattern=f"ايقاف_النشر ?(.*)")
 async def _(event):
     if (event.is_private or event.is_group):
-        return await edit_or_reply(event, "امر التلقائي يستخدم فقط للقنوات.")
-    hel_ = event.pattern_match.group(1)
-    if str(hel_).startswith("-100"):
-        jp = str(hel_).replace("-100", "")
+        return await edit_or_reply(event, "**᯽︙ عـذراً .. النشر التلقائي خـاص بالقنـوات فقـط**")
+    jok = event.pattern_match.group(1)
+    if not jok:
+        return await edit_or_reply(event, "**᯽︙ عـذراً .. قـم بـ إضـافة معـرف/ايـدي القنـاة الى الامـر اولاً**")
+    if jok.startswith("@"):
+        joker = jok
+    elif jok.startswith("https://t.me/"):
+        joker = jok.replace("https://t.me/", "@")
+    elif str(jok).startswith("-100"):
+        joker = str(jok).replace("-100", "")
     else:
-        jp = hel_
-    if not jp.isdigit():
-        return await edit_or_reply(event, "**رجاء قم بوضع ايدي القناه بجانب الأمر !!**")
-    if not is_post(jp, event.chat_id):
-        return await edit_or_reply(event, "هذا القناه لم يتم وضع تلقائي فيها.")
-    remove_post(jp, event.chat_id)
-    await edit_or_reply(event, f"**📍 تم ايقاف التلقائي لقناه : ** `{hel_}`")
+        try:
+            joker = int(jok)
+        except BaseException:
+            return await edit_or_reply(event, "**᯽︙ عـذراً .. معـرف/ايـدي القنـاة غيـر صـالح**\n**✾╎الرجـاء التـأكـد مـن المعـرف/الايـدي**")
+    try:
+        joker = (await event.client.get_entity(joker)).id
+    except BaseException:
+        return await event.reply("**᯽︙ عـذراً .. معـرف/ايـدي القنـاة غيـر صـالح**\n**✾╎الرجـاء التـأكـد مـن المعـرف/الايـدي**")
+    if not is_post(str(joker), event.chat_id):
+        return await edit_or_reply(event, "**᯽︙ تم تعطيـل النشر التلقـائي لهـذه القنـاة هنـا .. بنجـاح ✓**")
+    remove_post(str(joker), event.chat_id)
+    await edit_or_reply(event, f"**᯽︙ تم ايقـاف النشـر التلقـائي من** `{jok}`")
 
-@jepiq.ar_cmd(incoming=True)
+
+@jepiq.ar_cmd(incoming=True, forword=None)
 async def _(event):
     if event.is_private:
         return
@@ -48,4 +111,4 @@ async def _(event):
         if event.media:
             await event.client.send_file(int(chat), event.media, caption=event.text)
         elif not event.media:
-            await bot.send_message(int(chat), event.message)
+            await jepiq.send_message(int(chat), event.message)
