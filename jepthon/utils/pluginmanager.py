6cfb53c80dad159c260d133940591756a9d4ccfe
redaiq90 <60717660+redaiq90@users.noreply.github.com>
2022-11-09 07:58:04 +0300

    Update pluginmanager.py

--- jepthon/utils/pluginmanager.py
+++ jepthon/utils/pluginmanager.py
@@ -18,7 +18,7 @@ LOGS = logging.getLogger("jepthon")
 
 def load_module(shortname, plugin_path=None):
     if shortname.startswith("__"):
-        pass
+        name = "jepthon.ALJoker.{}".format(shortname)
     elif shortname.endswith("_"):
         path = Path(f"jepthon/plugins/{shortname}.py")
         checkplugins(path)
@@ -58,7 +58,8 @@ def load_module(shortname, plugin_path=None):
         spec.loader.exec_module(mod)
         # for imports
         sys.modules["jepthon.plugins." + shortname] = mod
-        LOGS.info("⌯︙تم بنجاح تحميل ملف " + shortname)
+        if shortname.startswith("jepthon."):
+            LOGS.info("⌯︙تم بنجاح تحميل ملف " + shortname)
 
 
 def remove_plugin(shortname):
