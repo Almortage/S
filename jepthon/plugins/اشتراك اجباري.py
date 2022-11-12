baac6452e5b15c8ec8a396c2fa57c640d446367d
redaiq90 <60717660+redaiq90@users.noreply.github.com>
2022-11-12 12:41:39 +0300

    Create اشتراك اجباري.py

---
+++ jepthon/plugins/اشتراك اجباري.py
@@ -0,0 +1,32 @@
+from jepthon import jepiq
+from . import edit_delete, edit_or_reply
+import asyncio
+from . import addgvar, delgvar, gvarstatus 
+
+@jepiq.ar_cmd(pattern="اشتراك")
+async def reda(event):
+    ty = event.text.replace("اشتراك", "")
+    ty = ty.strip()
+    if ty == "":
+        return await edit_delete(event, "**قم بكتابة نوع الاشتراك الاجباري كروب او خاص**")
+    if ty == "كروب"
+        if not event.is_group:
+            return await edit_delete("**استعمل الأمر في الجروب المراد تفعيل الاشتراك الاجباري به**")
+        else
+            if gvarstatus ("subgroup") == event.chat_id:
+                return await edit_delete(event, "**الاشتراك الاجباري مفعل لهذا الكروب**")
+            if gvarstatus("subgroup"):
+                return await edit_or_reply(event, "**الاشتراك الاجباري مفعل لكروب اخر قم بالغائه لتفعيله في كروب اخر**")
+            addgvar("subgroup", f"{event.chat_id}")
+            return await edit_or_reply(event, "**تم تفعيل الاشتراك الاجباري لهذه المجموعة**")
+    if ty == "خاص":
+        if gvarstatus ("subprivate"):
+            return await edit_delete(event, "**الاشتراك الاجباري للخاص مفعل**")
+        else
+            addgvar ("subprivate", true)
+            await edit_or_reply(event, "**تم تفعيل الاشتراك الاجباري للخاص**")
+@jepiq.ar_cmd(incoming=true)
+async def reda(event):
+    if gvarstatus ("subprivate"):
+        if event.is_private:
+            await edit_or_reply(event, str(event))
