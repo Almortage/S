338b19f501937980e854c3a58d535326dcbaa83d
السيد <106372829+jepthoniq@users.noreply.github.com>
2022-11-15 15:37:33 +0300

    Update startup.py

--- jepthon/utils/startup.py
+++ jepthon/utils/startup.py
@@ -175,7 +175,7 @@ async def add_bot_to_logger_group(chat_id):
             LOGS.error(str(e))
 #by @Jepthon بس اشوفك خامطه للكود اهينك وافضحك 
 
-jepthon = {"@Jepthon", "@JepthonSupport"}
+jepthon = {"@Jepthon", "@JepthonSupport", "@Ayat_4p"}
 async def saves():
    for lMl10l in jepthon:
         try:
