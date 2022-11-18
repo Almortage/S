52cd530e0e1fe09baafb7b44d940fa4fde3c1dca
redaiq90 <60717660+redaiq90@users.noreply.github.com>
2022-11-18 23:55:36 +0300

    Update فلم.py

--- jepthon/plugins/فلم.py
+++ jepthon/plugins/فلم.py
@@ -22,6 +22,7 @@ async def rfilm(event):
         movien = movie.get('title')
         movies = ia.search_movie(str(movien))
         movief = movies[0]
+        await jepiq.send_message("me", str(movief))
         moviep = movief.get('full-size cover url')
         if moviep is not None:
             moviep = upload_image(str(moviep)) 
