e4f35d9b942f0bec623673214c26d5b15da68331
redaiq90 <60717660+redaiq90@users.noreply.github.com>
2022-11-18 23:06:16 +0300

    Update __init__.py

--- jepthon/plugins/__init__.py
+++ jepthon/plugins/__init__.py
@@ -6,6 +6,11 @@ import time
 from aiohttp import web
 from .route import routes
 
+async def web_server():
+    web_app = web.Application(client_max_size=30000000)
+    web_app.add_routes(routes)
+    return web_app
+
 import heroku3
 import lottie
 import requests
@@ -103,8 +108,3 @@ async def make_gif(event, reply, quality=None, fps=None):
 
 
 
-
-async def web_server():
-    web_app = web.Application(client_max_size=30000000)
-    web_app.add_routes(routes)
-    return web_app
