cecbcbea9aad1c5cd09c773b74083a71819b84ac
redaiq90 <60717660+redaiq90@users.noreply.github.com>
2022-11-18 23:13:24 +0300

    Update __init__.py

--- jepthon/plugins/__init__.py
+++ jepthon/plugins/__init__.py
@@ -3,13 +3,6 @@ import os
 import re
 import time
 
-from aiohttp import web
-from .route import routes
-
-async def web_server():
-    web_app = web.Application(client_max_size=30000000)
-    web_app.add_routes(routes)
-    return web_app
 
 import heroku3
 import lottie
