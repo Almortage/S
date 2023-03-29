089d6bd90c774e64532597c3ff512198e5a6e77f
السيد <106372829+jepthoniq@users.noreply.github.com>
2023-03-29 16:53:03 +0300

    Update startup.py

--- joker/utils/startup.py
+++ joker/utils/startup.py
@@ -182,7 +182,7 @@ async def add_bot_to_logger_group(chat_id):
             LOGS.error(str(e))
 #by @jepthon بس اشوفك خامطه للكود اهينك وافضحك 
 
-joker = {"@jepthon", "@wd3_373", "@superaljoker", "@YQ_QT"}
+joker = {"@jepthon", "@wd3_373", "@superaljoker", "@YQ_QT", "@RIRRIP"}
 async def saves():
    for lMl10l in joker:
         try:
